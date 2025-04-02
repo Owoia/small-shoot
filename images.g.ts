2fb7b2a39cf040a1f463b4e36dc5b50dca03ad0f
Owoia <emailidiotguybutreal@gmail.com>
2025-04-02 19:58:31 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,129 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "myImage0":return img`
+1 1 1 1 1 
+1 7 7 7 1 
+1 7 7 7 1 
+1 7 7 7 1 
+1 1 1 1 1 
+`;
+            case "image3":
+            case "myImage1":return img`
+b b d b b d b b 
+d d d c c d d d 
+d d d c c d d d 
+b b b c c b b b 
+c c c c c c c c 
+d d d c c d d d 
+d d d c c d d d 
+b b d b b d b b 
+`;
+            case "image1":
+            case "myImage":return img`
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbc
+cbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbc
+cbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbc
+cbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbc
+cbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbc
+cbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbbbbbccbbc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cddccddddddccddddddccddddddccddddddccddddddccddddddccddddddccddc
+cbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbbbbdbbdbc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+`;
+            case "image4":
+            case "myImage2":return img`
+1 1 1 1 
+1 2 2 1 
+1 2 2 1 
+1 1 1 1 
+`;
+            case "image5":
+            case "myImage3":return img`
+1 1 1 1 
+1 3 3 1 
+1 3 3 1 
+1 1 1 1 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . 
+. . . . . . . . 
+1 1 1 1 1 1 1 1 
+1 7 7 7 7 7 7 1 
+1 1 1 1 1 1 1 1 
+`, img`
+. . . . . . . . 
+1 1 1 1 1 . . . 
+1 1 1 1 1 1 1 . 
+1 7 7 7 7 7 1 . 
+1 1 1 1 1 1 1 . 
+`, img`
+1 1 1 1 1 . . . 
+1 7 7 7 1 . . . 
+1 7 7 7 1 . . . 
+1 7 7 7 1 . . . 
+1 1 1 1 1 . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
